6f5296c53fcfd8ef47e87e8502cac67f4d02b5cd
Eric Lifka <eric.lifka@gmail.com>
2015-03-21 18:37:33 -0400

    point at resources folder for built files

---
+++ resources/app.js
@@ -0,0 +1,12 @@
+define(["exports", "module"], function (exports, module) {
+    "use strict";
+
+    var App = {
+        bootstrap: function bootstrap() {
+            return this;
+        }
+    };
+
+    module.exports = App.bootstrap();
+});
+//# sourceMappingURL=app.js.map
